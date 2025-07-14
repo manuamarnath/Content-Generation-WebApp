f999332c41651e060dc0c194a1643fff784c9bf3
MaNu <109624757+manuamarnath@users.noreply.github.com>
2025-07-14 21:51:48 +0530

    Update vite.config.js
    connect to backend

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5050'
+      '/api': 'https://content-generation-webapp-server.onrender.com'
     }
   }
 })
